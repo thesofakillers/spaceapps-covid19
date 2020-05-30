119da0af1a7742713fbe6985f248ac33a99eba1c
Haopeng <haopeng-li@outlook.com>
2020-05-30 17:19:50 +0100

    Adding "About" page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
